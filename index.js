a27a62e6c322e9b95feada5b0e698f242714cd06
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-05-01 03:46:57 +0600

    vercel added

--- index.js
+++ index.js
@@ -46,6 +46,12 @@ async function run() {
       const result = await productsCollection.findOne(query);
       res.send(result);
     });
+    app.get("/craftProduct/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    });
 
     app.get("/craftProduct/email/:email", async (req, res) => {
       const userEmail = req.params.email;
